d5e83860f2e8d4c424b7fa17592de66a9c7d87ca
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-08-21 16:58:08 -0700

    A poor typing tutor(it works though) a book of illusions(talked about anime, didn't get far)

--- src/_05_typing_tutor/TypingGame.java
+++ src/_05_typing_tutor/TypingGame.java
@@ -1,11 +1,14 @@
 package _05_typing_tutor;
 
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class TypingGame implements KeyListener {
 	
@@ -13,12 +16,15 @@ public class TypingGame implements KeyListener {
 	    Random r = new Random();
 	    return (char) (r.nextInt(26) + 'a');
 	}
+	 char currentLetter;
+	 JFrame f = new JFrame();
+		JLabel l = new JLabel();
+		JPanel p = new JPanel();
 void buttonSetup(){
-	JFrame f = new JFrame();
-	JLabel l = new JLabel();
-	char currentLetter;
+	
 	currentLetter = generateRandomLetter();
-	f.add(l);
+	f.add(p);
+	p.add(l);
 	f.setVisible(true);
 	l.setText(currentLetter+"");
 	l.setFont(l.getFont().deriveFont(28.0f));
@@ -29,7 +35,6 @@ void buttonSetup(){
 	public static void main(String[] args) {
 		TypingGame a = new TypingGame();
 		a.buttonSetup();
-		
 }
 	@Override
 	public void keyPressed(KeyEvent arg0) {
@@ -44,6 +49,16 @@ void buttonSetup(){
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		if()
+		if(arg0.getKeyChar()==currentLetter) {
+			
+			currentLetter = generateRandomLetter();
+			System.out.println("Correct");
+		p.setBackground(new Color(0, 255, 0));	
+		l.setText(currentLetter + "");
+		}
+		else {
+			System.out.println("Incorrect");
+			p.setBackground(new Color(255, 0, 0));	
+		}
 	}	
 }
