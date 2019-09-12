90e4959161d68a42bf9a4d028162689f72916584
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-09-11 17:02:51 -0700

    One more step to complete Twitter searcher

---
+++ src/_09_latest_tweet/TweetTweet.java
@@ -0,0 +1,64 @@
+package _09_latest_tweet;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class TweetTweet implements ActionListener {
+	static JFrame f = new JFrame();
+	static JPanel p = new JPanel();
+	static JTextField t = new JTextField();
+	static JButton b = new JButton();
+	public static void main(String[] args) {
+		TweetTweet tweet = new TweetTweet();
+		tweet.Setup();
+	}
+	 void Setup() {
+		f.add(p);
+		f.setVisible(true);
+		p.add(b);
+		p.add(t);
+		b.addActionListener(this);
+		b.setText("Search the TwitterVerse");
+		t.setPreferredSize(new Dimension(250, 25));
+		f.pack();
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		String search = t.getText();
+		getLatestTweet(search);
+	}
+	private String getLatestTweet(String searchingFor) {
+
+	    Twitter twitter = new TwitterFactory().getInstance();
+
+	    AccessToken accessToken = new AccessToken(
+	        "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+	        "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+	    twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+	        "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+	    twitter.setOAuthAccessToken(accessToken);
+
+	Query query = new Query(searchingFor);
+	    try {
+	        QueryResult result = twitter.search(query);
+	        return result.getTweets().get(0).getText();
+	    } catch (Exception e) {
+	        System.err.print(e.getMessage());
+	        return "What the heck is that?";
+	    }
+	}
+}
