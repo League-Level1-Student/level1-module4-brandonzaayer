0b912d23b2ef733dc8fe0ce7adad2d4256aff827
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-09 17:01:57 -0700

    That's not a mole. That's not a mole. That's not a mole.

--- src/_11_whack_a_mole/MoleSlapper.java
+++ src/_11_whack_a_mole/MoleSlapper.java
@@ -3,10 +3,12 @@ package _11_whack_a_mole;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
@@ -31,13 +33,17 @@ public class MoleSlapper implements ActionListener {
 	static JButton b16 = new JButton();
 	static JButton b17 = new JButton();
 	static JButton b18 = new JButton();
-	
+	JButton mole = new JButton();
+	int h = 0;
+	int rando;
+	Date d = new Date();
 	public static void main(String[] args) {
 		MoleSlapper slap = new MoleSlapper();
 		slap.Setup();
 	}
 
 void Setup() {
+	
 	f.add(p);
 	f.setVisible(true);
 	p.add(b1);
@@ -78,121 +84,111 @@ void Setup() {
 	b18.addActionListener(this);
 	f.setPreferredSize(new Dimension(250, 25));
 	Random rand = new Random();
-	int rando = rand.nextInt(15);;
+	mole.setText("");
+	 rando = rand.nextInt(15);;
 	if (rando == 0){
+		
 		b1.setText("Mole!");
+		mole=b1;
 	}
 	else if (rando == 1){
 		b2.setText("Mole!");
+		mole=b2;
 	}
 	else if (rando == 2){
+		
 		b3.setText("Mole!");
+		mole=b3;
 	}
 	else if (rando == 3){
 		b4.setText("Mole!");
+		mole=b4;
 	}
 	else if (rando == 4){
 		b5.setText("Mole!");
+		mole=b5;
 	}
 	else if (rando == 5){
 		b6.setText("Mole!");
+		mole=b6;
 	}
 	else if (rando == 6){
 		b7.setText("Mole!");
+		mole=b7;
 	}
 	else if (rando == 7){
 		b8.setText("Mole!");
+		mole=b8;
 	}
 	else if (rando == 8){
 		b9.setText("Mole!");
+		mole=b9;
 	}
 	else if (rando == 9){
 		b10.setText("Mole!");
+		mole=b10;
 	}
 	else if (rando == 10){
 		b11.setText("Mole!");
+		mole=b11;
 	}
 	else if (rando == 11){
 		b12.setText("Mole!");
+		mole=b12;
 	}
 	else if (rando == 12){
 		b13.setText("Mole!");
+		mole=b13;
 	}
 	else if (rando == 13){
 		b14.setText("Mole!");
+		mole=b14;
 	}
 	else if (rando == 14){
 		b15.setText("Mole!");
+		mole=b15;
 	}
 	else if (rando == 15){
 		b16.setText("Mole!");
+		mole=b16;
 	}
 	else if (rando == 16){
 		b17.setText("Mole!");
+		mole=b17;
 	}
 	else {
 		b18.setText("Mole!");
+		mole=b18;
 	}
 }
-
+public void hit() {
+	h++;
+	Setup();
+	if(h==10) {
+		endGame(d,10);
+	}
+}
+public void miss() {
+	speak("That's not a mole!");
+}
 
 @Override
 public void actionPerformed(ActionEvent arg0) {
 	// TODO Auto-generated method stub
-if(arg0.getSource().equals(b1) && b1.getText() == "Mole!") {
-		
+	JButton button = (JButton)arg0.getSource();
+if(button.getText().equals("Mole!")){
+	speak("That's a mole!");
+	hit();
 	}
-else if(arg0.getSource().equals(b2) && b2.getText() == "Mole!") {
-		
-	}
-else if(arg0.getSource().equals(b3) && b3.getText() == "Mole!") {
-		
-	}
-else if(arg0.getSource().equals(b4) && b4.getText() == "Mole!") {
-	
+else if(button.getText().equals("")){
+	miss();
 }
-else if(arg0.getSource().equals(b5) && b5.getText() == "Mole!") {
-	
 }
-else if(arg0.getSource().equals(b6) && b6.getText() == "Mole!") {
-	
-}
-else if(arg0.getSource().equals(b7) && b7.getText() == "Mole!") {
-	
-}
-else if(arg0.getSource().equals(b8) && b8.getText() == "Mole!") {
-
-}
-else if(arg0.getSource().equals(b9) && b9.getText() == "Mole!") {
-
-}
-else if(arg0.getSource().equals(b2) && b10.getText() == "Mole!") {
-	
-}
-else if(arg0.getSource().equals(b11) && b11.getText() == "Mole!") {
-	
-}
-else if(arg0.getSource().equals(b12) && b12.getText() == "Mole!") {
-
-}
-else if(arg0.getSource().equals(b13) && b13.getText() == "Mole!") {
-
-}
-else if(arg0.getSource().equals(b14) && b14.getText() == "Mole!") {
-
-}
-else if(arg0.getSource().equals(b15) && b15.getText() == "Mole!") {
-
-}
-else if(arg0.getSource().equals(b16) && b16.getText() == "Mole!") {
-
-}
-else if(arg0.getSource().equals(b17) && b17.getText() == "Mole!") {
-
-}
-else {
-	
-}
+private void endGame(Date timeAtStart, int molesWhacked) { 
+    Date timeAtEnd = new Date();
+    JOptionPane.showMessageDialog(null, "Your whack rate is "
+            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+                  + " moles per second.");
 }
 void speak(String words) { 
     try { 
